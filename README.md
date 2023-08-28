940714dbb5c77428fb13ecdb3a94cf929adfb65e
Jakub Janowski <jakub.janowski@nordsec.com>
2023-08-28 12:53:01 +0300

    Add support for pipeline cancellation
    When cancel-outdated-pipelines option is set to true
    the script will cancel all running pipelines that
    are running for the same github ref.

--- README.md
+++ README.md
@@ -8,19 +8,33 @@ This action triggers a pipeline for a specified project and ref at a specified G
 
 **Required** The root URL of the GitLab instance v4 API.
 
+### `access-token`
+
+**Required** A project access token (PAT) with Maintainer API access for the GitLab project.
+
+Instructions on creating access tokens: https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html#create-a-project-access-token
+
+### `trigger-token`
+
+**Required** A pipeline trigger token (PTT) for the GitLab project.
+
+Instructions on creating trigger tokens: https://docs.gitlab.com/ee/ci/triggers/#create-a-trigger-token
+
 ### `project-id`
 
 **Required** The ID of the project for which a pipeline shall be started.
 
-### `ref`
+### `triggered-ref`
 
 **Required** The ref of the project for which a pipeline shall be started.
 
-### `token`
+### `schedule`
 
-**Required** A trigger token for the GitLab project.
+**Optional** **[Default: false]** An indication if it is an automatically scheduled request.
 
-Instructions on creating tokens: https://docs.gitlab.com/ee/ci/triggers/#create-a-trigger-token
+### `cancel-outdated-pipelines`
+
+**Optional** **[Default: true]** If set to true, it will cancel previous pipelines that are running for the same reference.
 
 ## Outputs
 
@@ -29,10 +43,29 @@ None.
 ## Example usage
 
 ```yaml
-uses: NordSecurity/trigger-gitlab-pipeline@v1
+uses: NordSecurity/trigger-gitlab-pipeline@v2
 with:
   ci-api-v4-url: 'https://gitlab.com/api/v4/'
+  access-token: 'glpat-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  trigger-token: 'glptt-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   project-id: '1'
-  ref: 'main'
-  token: 'glptt-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  triggered-ref: 'main'
+```
+
+# Developement
+## Linters
+This project uses ESLint to lint the code. To run it locally install dev dependencies once with:
+```
+$npm install
+```
+and run eslint with:
+```
+$npx eslint ./index.js
+```
+## Updating node_modules
+Because this repository is a nodejs github action it should have node_modules that are needed to run the action committed into the repository.
+To not bloat the repository unnecessarily please only commit production dependencies.
+In order to install only production dependencies please run this before committing node_modules:
+```
+$npm install --omit=dev
 ```
--- index.js
+++ index.js
@@ -1,61 +1,188 @@
+'use strict';
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 const http = require('@actions/http-client');
 
-async function main() {
-    let sha = github.context.sha;
-
-    switch (github.context.eventName) {
-        case "pull_request":
-        case "pull_request_target":
-            sha = github.context.payload.pull_request.head.sha;
-        case "push":
-            break;
-        case "schedule":
-            break;
-
-        default:
-            core.debug('Not a push event, bailing out');
-
-            return;
+class GitlabClient {
+    constructor (inputConfig, githubConfig) {
+        this.inputConfig = inputConfig;
+        this.githubConfig = githubConfig;
+        this.client = new http.HttpClient();
     }
 
-    const repo = github.context.repo;
+    async request (method, url, data) {
+        let res;
 
-    const apiUrl = core.getInput('ci-api-v4-url', { required: true });
-    const id = core.getInput('project-id', { required: true });
-    const ref = core.getInput('ref', { required: true });
-    const token = core.getInput('token', { required: true });
-    const schedule = core.getBooleanInput('schedule', { required: false });
+        url.searchParams.append('access_token', this.inputConfig.accesToken);
 
-    const client = new http.HttpClient();
-    const url = new URL(`projects/${id}/trigger/pipeline`, apiUrl);
+        if (method === 'GET') {
+            res = await this.client.get(url);
+        } else if (method === 'POST') {
+            const additionalHeaders = {
+                'Content-Type': 'application/json'
+            };
+            res = await this.client.post(url, data, additionalHeaders);
+        } else {
+            throw new Error(`Unsupported method: ${method}`);
+        }
 
-    url.searchParams.append('token', token);
-    url.searchParams.append('ref', ref);
-    url.searchParams.append('variables[GITHUB_REF_NAME]', process.env.GITHUB_REF_NAME);
-    url.searchParams.append('variables[GITHUB_REF_TYPE]', process.env.GITHUB_REF_TYPE);
-    url.searchParams.append('variables[GITHUB_REPO]', `${repo.repo}`);
-    url.searchParams.append('variables[GITHUB_SHA]', sha);
-    url.searchParams.append('variables[GITHUB_SCHEDULE]', schedule);
+        if (res.message.statusCode !== 200 && res.message.statusCode !== 201) {
+            throw new Error(`API request [${method}] ${url.pathname} failed with code ${res.message.statusCode}`);
+        }
 
-    let res;
-    try {
-        res = await client.post(url);
-    } catch (e) {
-        core.setFailed('HTTP request failed');
-        return;
+        const body = await res.readBody();
+        return JSON.parse(body);
     }
 
-    if (res.message.statusCode != 201) {
-        core.setFailed(`API request failed with code ${res.message.statusCode}`);
-        return;
+    async get (url) {
+        return await this.request('GET', url);
     }
 
-    const body = await res.readBody();
-    const obj = JSON.parse(body);
+    async post (url, data) {
+        return await this.request('POST', url, data);
+    }
 
-    core.notice(obj.id);
+    async fetchRunningPipelinesIds () {
+        const fourHoursBefore = new Date();
+        fourHoursBefore.setHours(fourHoursBefore.getHours() - 4);
+
+        const url = new URL(`projects/${this.inputConfig.projectId}/pipelines`, this.inputConfig.apiUrl);
+        url.searchParams.append('ref', this.inputConfig.triggeredRef);
+        url.searchParams.append('status', 'running');
+        url.searchParams.append('per_page', '100');
+        url.searchParams.append('updated_after', fourHoursBefore.toISOString());
+
+        const pipelines = await this.get(url);
+        return pipelines.map(pipeline => pipeline.id);
+    }
+
+    async filterPipelinesByGithubRef (pipelineIds) {
+        const requests = pipelineIds.map(pipelineId => {
+            const url = new URL(
+                `projects/${this.inputConfig.projectId}/pipelines/${pipelineId}/variables`,
+                this.inputConfig.apiUrl
+            );
+            return this.get(url);
+        });
+        const variables = await Promise.all(requests);
+
+        return pipelineIds.filter((_, index) => {
+            const githubRefName = variables[index].find(variable => variable.key === 'GITHUB_REF_NAME');
+            const githubRefType = variables[index].find(variable => variable.key === 'GITHUB_REF_TYPE');
+            return githubRefName && githubRefType &&
+                githubRefName.value === this.githubConfig.githubRefName &&
+                githubRefType.value === this.githubConfig.githubRefType;
+        });
+    }
+
+    async cancelPreviousPipelines (pipelineIds) {
+        const requests = pipelineIds.map(pipelineId => {
+            const url = new URL(
+                `projects/${this.inputConfig.projectId}/pipelines/${pipelineId}/cancel`,
+                this.inputConfig.apiUrl
+            );
+            return this.post(url);
+        });
+        await Promise.all(requests);
+    }
+
+    async triggerPipeline () {
+        const url = new URL(`projects/${this.inputConfig.projectId}/trigger/pipeline`, this.inputConfig.apiUrl);
+
+        url.searchParams.append('token', this.inputConfig.triggerToken);
+        url.searchParams.append('ref', this.inputConfig.triggeredRef);
+        url.searchParams.append('variables[GITHUB_REF_NAME]', this.githubConfig.githubRefName);
+        url.searchParams.append('variables[GITHUB_REF_TYPE]', this.githubConfig.githubRefType);
+        url.searchParams.append('variables[GITHUB_REPO]', this.githubConfig.githubRepo);
+        url.searchParams.append('variables[GITHUB_SHA]', this.githubConfig.githubSha);
+        url.searchParams.append('variables[GITHUB_SCHEDULE]', this.inputConfig.schedule);
+
+        return await this.post(url);
+    }
 }
 
-main();
+async function execute (inputConfig, githubConfig) {
+    const gitlabClient = new GitlabClient(inputConfig, githubConfig);
+
+    if (inputConfig.cancelOutdatedPipelines) {
+        const pipelineIds = await gitlabClient.fetchRunningPipelinesIds();
+        const filteredPipelineIds = await gitlabClient.filterPipelinesByGithubRef(pipelineIds);
+        await gitlabClient.cancelPreviousPipelines(filteredPipelineIds);
+    }
+
+    const resp = await gitlabClient.triggerPipeline();
+    return resp.id;
+}
+
+async function main () {
+    const inputConfig = {
+        apiUrl: core.getInput('ci-api-v4-url', { required: true }),
+        accesToken: core.getInput('access-token', { required: true }),
+        triggerToken: core.getInput('trigger-token', { required: true }),
+        projectId: core.getInput('project-id', { required: true }),
+        triggeredRef: core.getInput('triggered-ref', { required: true }),
+        schedule: core.getBooleanInput('schedule', { required: false }),
+        cancelOutdatedPipelines: core.getBooleanInput('cancel-outdated-pipelines', { required: false })
+    };
+
+    const githubConfig = {
+        githubRefName: process.env.GITHUB_REF_NAME,
+        githubRefType: process.env.GITHUB_REF_TYPE,
+        githubRepo: github.context.repo.repo,
+        githubSha: github.context.sha
+    };
+
+    switch (github.context.eventName) {
+    case 'pull_request':
+    case 'pull_request_target':
+        githubConfig.githubSha = github.context.payload.pull_request.head.sha;
+        break;
+    case 'push':
+    case 'schedule':
+        break;
+    default:
+        throw new Error(`Unsupported triggering event: ${github.context.eventName}`);
+    }
+
+    Object.freeze(inputConfig);
+    Object.freeze(githubConfig);
+
+    core.notice(await execute(inputConfig, githubConfig));
+}
+
+if (github.context.sha) {
+    main().catch(e => {
+        console.log(e);
+        core.setFailed(e.toString());
+    });
+} else {
+    // This is executed when running locally and can be used for testing
+    const inputConfig = {
+        apiUrl: '',
+        accesToken: '',
+        triggerToken: '',
+        projectId: '',
+        triggeredRef: '',
+        schedule: false,
+        cancelOutdatedPipelines: true
+    };
+
+    const githubConfig = {
+        githubRefName: '',
+        githubRefType: '',
+        githubRepo: '',
+        githubSha: ''
+    };
+
+    Object.freeze(inputConfig);
+    Object.freeze(githubConfig);
+
+    execute(inputConfig, githubConfig).then(
+        res => {
+            console.log(res);
+        }
+    ).catch(e => {
+        console.log(e);
+    });
+}
